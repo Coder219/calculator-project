293e6f348f49eb5ecade21f7acff20c03c2f5e31
Coder219 <145808304+Coder219@users.noreply.github.com>
2024-08-26 18:17:19 -0400

    Added a basic display sys

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,41 @@
 const display = document.querySelector(".display");
+const numpad = document.querySelector(".numpad");
+const numpadRows = document.querySelector(".numpad-row");
+const numbers = document.querySelectorAll(".num");
+const operators = document.querySelector(".operators");
+const evalButton = document.querySelector(".eval");
+const clearButton = document.querySelector(".clear");
+
+let operands = [[],[]];
+let currentNum = 0;
+let operator = '';
+let result = '';
+
+function displayScreen(){
+    display.textContent = operands[0] + ' ' + operator + ' ' + operands[1];
+}
+
+numpad.addEventListener("click", function(event) {
+    operands[currentNum].push(parseInt(event.target.id));
+    operands[currentNum][0] = parseInt(operands[currentNum].join(''));
+    operands[currentNum].splice(1,1) 
+    console.log(operands);
+    displayScreen();
+});
+
+operators.addEventListener("click", function(event) {
+    operator = event.target.id;
+    currentNum += 1;
+    displayScreen();
+});
+
+clearButton.addEventListener("click", function(event) {
+    operands = [[],[]];
+    operator = '';
+    console.log(operands);
+    displayScreen();
+});
+
+evalButton.addEventListener('click', function(event) {
+
+})
